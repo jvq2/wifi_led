54b7407cc54ed5d5ca3c3ea3bcbbb2955ee1ce72
Joey <joey.v.q@gmail.com>
2018-02-21 20:26:25 -0500

    wip

--- led_controller.py
+++ led_controller.py
@@ -13,9 +13,43 @@ def hello():
 
 def on():
 	return cmd('71230fa3')
+	# response is f0:71:23:84
 
 def off():
 	return cmd('71240fa4')
+	# response is f0:71:24:85
+
+def rgbw(r, g, b, w):
+	# 31:fd:fd:fd:ff:00:0f:36
+	# 31:f7:f7:f7:ff:00:0f:24
+	# 31:f4:f4:f4:ff:00:0f:1b
+	# 31:f3:f3:f3:ff:00:0f:18
+	# 31:f2:f2:f2:ff:00:0f:15
+	# 31:f1:f1:f1:ff:00:0f:12
+	# 31:f0:f0:f0:ff:00:0f:0f  <--- middle
+	# 31:f5:f5:f5:ff:00:0f:1e
+	# 31:fa:fa:fa:ff:00:0f:2d
+	# 31:fe:fe:fe:ff:00:0f:39
+	# 31:ff:ff:ff:ff:00:0f:3c
+
+	# 31:06:06:06:ff:00:0f:51
+	# 31:01:01:01:ff:00:0f:42
+	# 31:00:00:00:ff:00:0f:3f
+
+	# 31:fc:fc:fc:ff:00:0f:33
+	# 31:fe:fe:fe:ff:00:0f:39
+	# 31:fb:fb:fb:ff:00:0f:30:31:f9:f9:f9:ff:00:0f:2a   <--- wtf
+	# 31:f7:f7:f7:ff:00:0f:24
+	# 31:f3:f3:f3:ff:00:0f:18
+	# 31:ef:ef:ef:ff:00:0f:0c
+	# 31:e8:e8:e8:ff:00:0f:f7
+	# 31:e7:e7:e7:ff:00:0f:f4
+	# ...
+
+	# 31:00:00:00:ff:00:0f:3f
+	# 31:00:00:00:00:00:0f:40
+	return cmd('31''fdfdfd''ff''000f36')
+	# response 30
 
 
 HOST = '192.168.1.147'    # The remote host
