359757a3549310e96fe2472e10b0ad8cc10a958e
Joey <joey.v.q@gmail.com>
2018-03-15 19:56:34 -0400

    Add connect

--- led_controller.py
+++ led_controller.py
@@ -2,7 +2,15 @@ import socket
 import time
 
 
-def send(hex_str):
+HOST = '192.168.1.147'
+PORT = 5577
+
+def connect(host, port):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((host, port))
+	return sock
+
+def send(s, hex_str):
 	s.send(hex_str.decode('hex'))
 
 def calc_checksum(hex_str):
@@ -11,52 +19,47 @@ def calc_checksum(hex_str):
 		total += int(hex_str[i:i+2], 16)
 	return hex(total)[-2:]
 
-def cmd(hex_str):
+def cmd(s, hex_str):
 	hex_str += calc_checksum(hex_str)
-	send(hex_str)
+	send(s, hex_str)
 	data = s.recv(1024)
 	print 'Received', repr(data)
 	return data
 
-def hello():
-	return cmd('818a8b')
+def hello(s):
+	return cmd(s, '818a8b')
 
-def on():
-	return cmd('71230f')
+def on(s):
+	return cmd(s, '71230f')
 	# response is f0:71:23:84
 
-def off():
-	return cmd('71240f')
+def off(s):
+	return cmd(s, '71240f')
 	# response is f0:71:24:85
 
-def rgbw(r, g, b, w):
+def rgbw(sock, r, g, b, w):
 	hx = '31{r}{g}{b}{w}000f'.format(r=r, g=g, b=b, w=w)
-	return cmd(hx)
+	return cmd(sock, hx)
 	# response 30
 
-
-
-HOST = '192.168.1.147'    # The remote host
-PORT = 5577              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s = connect(HOST, PORT)
 
 # Say hello. This same greeting seems to be used for every connection
 # cmd('818a8b96')
-hello()
+hello(s)
 
 # setting a color
 # cmd('31f1f1f1ff000f12')
 
 
 # on
-on()
+on(s)
 
 # time.sleep(5)
 
 # off
 # cmd('71240fa4')
-# off()
+# off(s)
 
 s.close()
 # 81:04$a:01:10:ff:ff:ff:ff:04:00:00:1b
