5a0c8d4f02e1a67d02bf4e8d78b0127cd8494d7c
Joey <joey.v.q@gmail.com>
2018-04-02 18:38:18 -0400

    Add read only properties for light state

--- led_controller.py
+++ led_controller.py
@@ -6,18 +6,32 @@ class LEDController():
 
 	def __init__(self):
 		self._socket = None
-		self.on = False
-		self.r = 0
-		self.g = 0
-		self.b = 0
-		self.w = 0
+		self._commands = {129: self._parse_status}
+		self._on = False
+		self._r = 0
+		self._g = 0
+		self._b = 0
+		self._w = 0
 
-	def connect(self, host, port):
-		self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self._socket.connect((host, port))
+	@property
+	def on(self):
+		return self._on
 
-	def close(self):
-		self._socket.close()
+	@property
+	def r(self):
+		return self._r
+
+	@property
+	def g(self):
+		return self._g
+
+	@property
+	def b(self):
+		return self._b
+
+	@property
+	def w(self):
+		return self._w
 
 	def _send(self, hex_str):
 		self._socket.send(hex_str.decode('hex'))
@@ -35,53 +49,73 @@ class LEDController():
 		print 'Received', repr(data)
 		return data
 
-	def _calc_checksum(self, hex_str):
+	def _calc_str_checksum(self, hex_str):
 		total = 0
 		for i in range(0, len(hex_str), 2):
 			total += int(hex_str[i:i+2], 16)
 		return hex(total)[-2:]
 
-	def _parse_hello(self, data):
+	def _response_is_valid(self, data):
+		checksum = int(hex(sum(data[:-1]))[-2:], 16)
+		return checksum == data[-1]
+
+	def _parse_status(self, data):
 		# \x81\x04$a\x01\x08\xf1\xf1\xf1\xff\x04\x00\x00\xe9 - off
 		# \x81\x04#a\x01\x08\xf1\xf1\xf1\xff\x04\x00\x00\xe8 - on
-		self.on = data[2] == 35
-		self.r = data[6]
-		self.g = data[7]
-		self.b = data[8]
-		self.w = data[9]
+		self._on = data[2] == 35
+		self._r = data[6]
+		self._g = data[7]
+		self._b = data[8]
+		self._w = data[9]
 
 	def _byte_str_to_list(self, data):
 		return [ord(char) for char in data]
 
 	def _parse_response(self, data):
-		commands = {'\x81': self._parse_hello}
+		data = self._byte_str_to_list(data)
+		if not self._response_is_valid(data):
+			print 'invalid response checksum'
+			return
+
 		prefix = data[0]
 
-		command = commands.get(prefix, None)
-		data = self._byte_str_to_list(data)
-		if command is not None:
-			command(data)
+		command = self._commands.get(prefix, self._do_nothing)
+		command(data)
+
+	def _do_nothing(self, data=None):
+		print 'Unrecognised response: {}'.format(data)
+
+	def connect(self, host, port):
+		self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self._socket.connect((host, port))
+		self.get_status()
+
+	def close(self):
+		self._socket.close()
 
 	def send_cmd(self, hex_str):
-		hex_str += self._calc_checksum(hex_str)
+		hex_str += self._calc_str_checksum(hex_str)
 		self._send(hex_str)
 		data = self._receive()
+		self._parse_response(data)
 		return data
 
-	def hello(self):
-		return self.cmd('818a8b')
+	def get_status(self):
+		return self.send_cmd('818a8b')
 
-	def on(self):
-		return self.cmd('71230f')
+	def power_on(self):
+		return self.send_cmd('71230f')
 		# response is f0:71:23:84
+		# [240, 113, 35, 132]
 
-	def off(self):
-		return self.cmd('71240f')
+	def power_off(self):
+		return self.send_cmd('71240f')
 		# response is f0:71:24:85
+		# [240, 113, 36, 133]
 
 	def rgbw(self, r, g, b, w):
 		hex_str = '31{r}{g}{b}{w}000f'.format(r=r, g=g, b=b, w=w)
-		return self.cmd(hex_str)
+		return self.send_cmd(hex_str)
 		# response 30
 
 
@@ -89,11 +123,10 @@ if __name__ == '__main__':
 	controller = LEDController()
 
 	controller.connect('192.168.1.147', 5577)
-	controller.hello()
 
-	controller.on()
-	time.sleep(5)
-	controller.off()
+	controller.power_on()
+	# time.sleep(5)
+	# controller.power_off()
 
 	controller.close()
 
